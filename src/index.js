4cc0b4e3a0009eae7b3f36fc2fab9d86adeefb32
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-14 22:58:40 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+	const form = document.querySelector("form");
+	form.addEventListener("submit", (event) => {
+	  event.preventDefault();
+	  const input = form.querySelector("#new-task-description").value;
+	  submitTodo(input)
+	  form.reset()
+	})
+  });
+  
+  const submitTodo = (todo) => {
+	const ul = document.querySelector("#tasks")
+	const li = document.createElement("li");
+	const btn = document.createElement("button");
+	btn.addEventListener("click", deleteList)
+	btn.innerHTML = "<strong>DELETE</strong>";
+	li.textContent =`${todo} `;
+	li.appendChild(btn)
+	ul.appendChild(li);
+	
+  }
+  
+  const deleteList = (event) => {
+	 event.target.parentNode.parentNode.remove();
+  }
+  
